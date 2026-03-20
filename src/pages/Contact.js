ee87eb94380256625638ef2ba360d6fda9e3ace7
bhaveshpatil-source <bhavesh.patil@makemyveggies.com>
2026-03-20 17:10:29 +0530

    i have update the API's endpoint

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import { toast } from 'react-toastify';
 
-const API_BASE = process.env.REACT_APP_API_BASE || 'https://dev.makemyveggies.com/';
+const API_BASE = process.env.REACT_APP_API_BASE || 'https://makemyveggies.com/';
 
 const Contact = () => {
   const [form, setForm] = useState({
